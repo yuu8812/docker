49816f0ff6d3dd568d4102c039d46337ae6f152a
こんにちは <konnichiha@konnichihanoMacBook-Air.local>
2022-11-11 15:04:44 +0900

    second commit

--- nodeDocker/src/routes/users.js
+++ nodeDocker/src/routes/users.js
@@ -1,6 +1,16 @@
 var express = require('express');
 var router = express.Router();
 
+// const mysql = require('mysql');
+
+// const connection = mysql.createConnection({
+//   host: 'localhost:3000',
+//   user: 'foir075@gmail.com',
+//   database: 'convinience',
+// });
+
+// connection.connect();
+
 /* GET users listing. */
 router.get('/', function (req, res, next) {
   res.send('respond with a resources');
